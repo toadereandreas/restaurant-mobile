044aa927ce7a48c79dc2a4ba4f04433dbde53c2d
Horațiu Udrea <udrea.horatiu@gmail.com>
2020-10-17 12:27:30 +0300

    Initial Commit

---
+++ app/src/main/java/com/gradysbooch/restaurant/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.gradysbooch.restaurant
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
+import com.gradysbooch.restaurant.ui.RestaurantmobileTheme
+
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RestaurantmobileTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String)
+{
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview()
+{
+    RestaurantmobileTheme {
+        Greeting("Android")
+    }
+}
