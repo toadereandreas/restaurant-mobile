76770ef3a5c5d87425afdd27663ae381b7f85d0a
undoureche <undoureche@gmail.com>
2020-11-09 21:46:28 +0200

    Link is fine. Request querry function is done, awaiting test.

--- app/src/main/java/com/gradysbooch/restaurant/repository/NetworkRepository.kt
+++ app/src/main/java/com/gradysbooch/restaurant/repository/NetworkRepository.kt
@@ -5,9 +5,13 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.coroutines.toDeferred
+import com.gradysbooch.restaurant.GetMenuItemDetailsQuery
+
+import kotlinx.coroutines.runBlocking
 
 val apolloClient = ApolloClient.builder()
-    .serverUrl("http://restaurant-env.eba-prznaj7v.eu-west-3.elasticbeanstalk.com/graphql/") //todo take a look at this URL, it's unlikely that this is the right one
+    .serverUrl("http://restaurant-env.eba-prznaj7v.eu-west-3.elasticbeanstalk.com/graphql/")
     .build()
 
 class NetworkRepository(context: Context) : NetworkRepositoryInterface
@@ -16,4 +20,19 @@ class NetworkRepository(context: Context) : NetworkRepositoryInterface
     val internalOnlineStatus = MutableStateFlow(false)
 
     override val onlineStatus: Flow<Boolean> = internalOnlineStatus
+
+    /**
+     * This is a test function
+     * Murderize it when you don't need it anymore
+     *
+     * This might return null, watch out
+     */
+    suspend fun getMenuItem(id: Int) : GetMenuItemDetailsQuery.MenuItem?{
+        val menuItem : GetMenuItemDetailsQuery.MenuItem?;
+
+        menuItem = apolloClient.query(GetMenuItemDetailsQuery(id.toString())).toDeferred().await().data?.menuItem
+
+
+        return menuItem;
+    }
 }
