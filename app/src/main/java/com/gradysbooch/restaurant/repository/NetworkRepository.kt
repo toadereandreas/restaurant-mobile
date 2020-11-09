18b9ebaf5d4175d44614ffc947b28a778ddaf50d
undoureche <undoureche@gmail.com>
2020-11-09 19:42:36 +0200

    Added basic graphql files. Currently empty, will work on the schema first.

--- app/src/main/java/com/gradysbooch/restaurant/repository/NetworkRepository.kt
+++ app/src/main/java/com/gradysbooch/restaurant/repository/NetworkRepository.kt
@@ -4,6 +4,11 @@ import android.content.Context
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
+import com.apollographql.apollo.ApolloClient
+
+val apolloClient = ApolloClient.builder()
+    .serverUrl("https://apollo-fullstack-tutorial.herokuapp.com")
+    .build()
 
 class NetworkRepository(context: Context) : NetworkRepositoryInterface
 {
