ebba7d02707b27b8826379a6e0df828d17fb01d4
undoureche <undoureche@gmail.com>
2020-11-09 21:48:23 +0200

    less clutter?

--- app/src/main/java/com/gradysbooch/restaurant/repository/NetworkRepository.kt
+++ app/src/main/java/com/gradysbooch/restaurant/repository/NetworkRepository.kt
@@ -5,10 +5,8 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import com.apollographql.apollo.ApolloClient
-import com.apollographql.apollo.coroutines.toDeferred
+import com.apollographql.apollo.coroutines.await
 import com.gradysbooch.restaurant.GetMenuItemDetailsQuery
-
-import kotlinx.coroutines.runBlocking
 
 val apolloClient = ApolloClient.builder()
     .serverUrl("http://restaurant-env.eba-prznaj7v.eu-west-3.elasticbeanstalk.com/graphql/")
@@ -28,11 +26,8 @@ class NetworkRepository(context: Context) : NetworkRepositoryInterface
      * This might return null, watch out
      */
     suspend fun getMenuItem(id: Int) : GetMenuItemDetailsQuery.MenuItem?{
-        val menuItem : GetMenuItemDetailsQuery.MenuItem?;
 
-        menuItem = apolloClient.query(GetMenuItemDetailsQuery(id.toString())).toDeferred().await().data?.menuItem
+        return apolloClient.query(GetMenuItemDetailsQuery(id.toString())).await().data?.menuItem
 
-
-        return menuItem;
     }
 }
