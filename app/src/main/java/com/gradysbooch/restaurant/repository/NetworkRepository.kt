5f7ccd94f6a8ceb32fba7002fae1ab7523b9d99a
Undo <undoureche@gmail.com>
2020-11-15 18:50:29 +0200

    Reqeust for menuItems

--- app/src/main/java/com/gradysbooch/restaurant/repository/NetworkRepository.kt
+++ app/src/main/java/com/gradysbooch/restaurant/repository/NetworkRepository.kt
@@ -1,17 +1,23 @@
 package com.gradysbooch.restaurant.repository
 
 import android.content.Context
+import android.util.Log
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import com.apollographql.apollo.ApolloClient
 import com.apollographql.apollo.coroutines.await
+import com.apollographql.apollo.exception.ApolloException
 import com.gradysbooch.restaurant.GetMenuItemDetailsQuery
+import com.gradysbooch.restaurant.GetMenuItemsQuery
 import com.gradysbooch.restaurant.model.MenuItem
 import com.gradysbooch.restaurant.model.Table
+import java.io.IOException
+import kotlin.math.log
+import kotlin.math.roundToInt
 
 val apolloClient = ApolloClient.builder()
-    .serverUrl("http://restaurant-env.eba-prznaj7v.eu-west-3.elasticbeanstalk.com/graphql/")
+    .serverUrl("https://restaurant.playgroundev.com/graphql")
     .build()
 
 class NetworkRepository(context: Context) : NetworkRepositoryInterface
@@ -22,7 +28,27 @@ class NetworkRepository(context: Context) : NetworkRepositoryInterface
     override val onlineStatus: Flow<Boolean> = internalOnlineStatus
 
     override suspend fun getMenuItems(): Set<MenuItem> {
-        TODO("Not yet implemented")
+        val set = mutableSetOf<MenuItem>();
+
+        try{
+            val list = apolloClient.query(GetMenuItemsQuery()).await().data?.menuItems?.data;
+
+            var badId = 0;//todo Fix this badId thing. it should be received from the backend
+            list?.forEach {
+                if (it != null)
+                    set.add(MenuItem(badId++, it.internalName, it.category.internalName, it.price.roundToInt()))
+            }
+
+            internalOnlineStatus.emit(true);
+
+        }catch (e : ApolloException){
+            internalOnlineStatus.emit(true);
+            Log.d("ApolloError", e.stackTraceToString());
+
+            throw IOException("ApolloFailure: failed to get menu items. Exception is: ${e.stackTraceToString()}");
+        }
+
+        return set;
     }
 
     override suspend fun getTables(): Set<Table> {
