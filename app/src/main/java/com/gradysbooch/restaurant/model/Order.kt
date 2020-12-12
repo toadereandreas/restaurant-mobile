85813548638063437dba199abef7011e44441e44
Undo <undoureche@gmail.com>
2020-12-12 21:22:02 +0200

    Lorem ipsum DOLOR!
    Just made sure to add serialized names to the model items, for Gson. Some of them are redundant, but lorem ipsum.. Either way, no @Expose constraints yet
    
    todo: same as previous commit -> make sure every socket callback is on a different thread then remove the coroutine (replace with runblocking scope)

--- app/src/main/java/com/gradysbooch/restaurant/model/Order.kt
+++ app/src/main/java/com/gradysbooch/restaurant/model/Order.kt
@@ -4,9 +4,13 @@ import androidx.room.Entity
 import androidx.room.ForeignKey
 import androidx.room.ForeignKey.CASCADE
 import androidx.room.PrimaryKey
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
 
 @Entity(primaryKeys = ["orderColor", "tableUID"])
 data class Order(
+
+    @SerializedName("tableId")
     @ForeignKey(
         entity = Table::class,
         parentColumns = ["UID"],
@@ -15,6 +19,10 @@ data class Order(
         onUpdate = CASCADE
     )
     val tableUID: String,
+
+    @SerializedName("orderColor")
     val orderColor: String,
+
+    @SerializedName("note")
     val note: String
 )
