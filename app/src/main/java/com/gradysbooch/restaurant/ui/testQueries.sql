a97ec93598862745d4e1e049a5635b8e85c33935
iosifsecheleadan <iosifsecheleadan@protonmail.com>
2021-01-02 21:11:39 +0200

    test queries

---
+++ app/src/main/java/com/gradysbooch/restaurant/ui/testQueries.sql
@@ -0,0 +1,4 @@
+insert into `Table` (tableUID, name, code, call) values  ('0', 'table0', '123', '13');
+insert into MenuItem (menuItemUID, name, price) values ('0', 'menu item 0');
+insert into `Order` (tableUID, orderColor, note) values ('0', '#f00', 'order 0 note');
+insert into OrderItem (orderColor, tableUID, menuItemUID, quantity) values ('#0f0', '0', '0', '3');
